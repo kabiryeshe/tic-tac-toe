b7caf40f1d06023422936d996dd7f8e889e2f882
Tushar <tusharg@thoughtworks.com>
2016-05-15 13:51:52 +0530

    added test for Board model

--- TicTacToe/Model/TileLocation.m
+++ TicTacToe/Model/TileLocation.m
@@ -16,4 +16,18 @@
     return [[TileLocation alloc]initWithRow:row column:column];
 }
 
+
+- (BOOL)isEqual:(id)object {
+    TileLocation *anotherLocation = object;
+    return (self == anotherLocation) || (self.row == anotherLocation.row && self.column == anotherLocation.column);
+}
+
+- (NSUInteger)hash{
+    NSInteger prime = 31;
+    NSInteger result = 1;
+    result = prime * result + self.row;
+    result = prime * result + self.column;
+    return result;
+}
+
 @end
--- TicTacToeTests/Model/BoardTestCase.m
+++ TicTacToeTests/Model/BoardTestCase.m
@@ -1,8 +1,14 @@
 
 #import <XCTest/XCTest.h>
+#import <OCHamcrest/OCHamcrest.h>
 #import "Board.h"
+#import "TileLocation.h"
 
-@interface BoardTestCase : XCTestCase
+
+@interface BoardTestCase : XCTestCase {
+    NSString *mark1;
+    NSString *mark2;
+}
 
 @end
 
@@ -11,24 +17,76 @@
 - (void)setUp {
     [super setUp];
     
+    mark1 = @"0";
+    mark2 = @"X";
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testShouldReturnRowWithSameMarking {
+- (void)testThatItReturnsRowWhenItsAllMarkingsAreSame {
     
-    NSString *mark1 = @"O";
-    NSString *mark2 = @"X";
     Board* board = [[Board alloc]initWithTiles:@[
                                 @[mark1, mark2, mark1],
                                 @[mark2, mark2, mark2],
                                 @[mark1, mark1, mark2]
                                 ]];
-    NSArray *markLocations = [board checkForThreeContinuousMarks];
-    XCTAssertNotNil(markLocations);
+    NSArray* markLocations = [board checkForThreeContinuousMarks];
+    
+    NSArray* expectedMarkings = @[[TileLocation locationWithRow:1 column:0],
+                                 [TileLocation locationWithRow:1 column:1],
+                                 [TileLocation locationWithRow:1 column:2]];
+    
+    assertThat(markLocations, containsIn(expectedMarkings));
 
 }
+
+
+- (void)testThatItReturnsColumnWhenItsAllMarkingsAreSame {
+    
+    Board* board = [[Board alloc]initWithTiles:@[
+                                                 @[mark1, mark2, mark1],
+                                                 @[mark2, mark2, mark1],
+                                                 @[mark1, mark2, mark2]
+                                                 ]];
+    
+    NSArray* markLocations = [board checkForThreeContinuousMarks];
+    
+    NSArray* expectedMarkings = @[[TileLocation locationWithRow:0 column:1],
+                                  [TileLocation locationWithRow:1 column:1],
+                                  [TileLocation locationWithRow:2 column:1]];
+    assertThat(markLocations, containsIn(expectedMarkings));
+}
+
+
+- (void)testThatItReturnsLeftDiagonalWhenItsAllMarkingsAreSame {
+    
+    Board* board = [[Board alloc]initWithTiles:@[
+                                                 @[mark1, mark2, mark1],
+                                                 @[mark2, mark1, mark2],
+                                                 @[mark2, mark2, mark1]
+                                                 ]];
+    
+    NSArray* markLocations = [board checkForThreeContinuousMarks];
+    
+    NSArray* expectedMarkings = @[[TileLocation locationWithRow:0 column:0],
+                                  [TileLocation locationWithRow:1 column:1],
+                                  [TileLocation locationWithRow:2 column:2]];
+    assertThat(markLocations, containsIn(expectedMarkings));
+}
+
+- (void)testThatItReturnsRightDiagonalWhenItsAllMarkingsAreSame {
+    
+    Board* board = [[Board alloc]initWithTiles:@[
+                                                 @[mark1, mark2, mark1],
+                                                 @[mark2, mark1, mark2],
+                                                 @[mark1, mark2, mark2]
+                                                 ]];
+    
+    NSArray* markLocations = [board checkForThreeContinuousMarks];
+    
+    NSArray* expectedMarkings = @[[TileLocation locationWithRow:0 column:2],
+                                  [TileLocation locationWithRow:1 column:1],
+                                  [TileLocation locationWithRow:2 column:0]];
+    assertThat(markLocations, containsIn(expectedMarkings));
+}
+
+
 @end
